f151444f1463aa5247be51d46a0371e06fe0fb6b
OMOLEBILL <105784385+OMOLEBILL@users.noreply.github.com>
2022-08-10 21:41:47 +0200

    group

--- _printf.c
+++ _printf.c
@@ -1,21 +1,20 @@
 #include "main.h"
 /**
- * _printf - function that prints based on format specifier
- * @format: takes in format specifier
- * Return: return pointer to index
- **/
+  * _printf - function that prints based on format specifier
+  * @format: takes in format specifier
+  * Return: return pointer to index
+  */
 int _printf(const char *format, ...)
 {
 	char buffer[1024];
 	int i, j = 0, a = 0, *index = &a;
 	va_list valist;
 	vtype_t spec[] = {
-	{'c', format_c}, {'d', format_d}, {'s', format_s}, {'i', format_d},
-	{'u', format_u}, {'%', format_perc}, {'x', format_h}, {'X', format_ch},
-	{'o', format_o}, {'b', format_b}, {'p', format_p}, {'r', format_r},
-	{'R', format_R}, {'\0', NULL}
+		{'c', format_c}, {'d', format_d}, {'s', format_s}, {'i', format_d},
+		{'u', format_u}, {'%', format_perc}, {'x', format_h}, {'X', format_ch},
+		{'o', format_o}, {'b', format_b}, {'p', format_p}, {'r', format_r},
+		{'R', format_R}, {'\0', NULL}
 	};
-
 	if (!format)
 		return (-1);
 	va_start(valist, format);
@@ -24,8 +23,7 @@ int _printf(const char *format, ...)
 		for (; format[i] != '%' && format[i] != '\0'; *index += 1, i++)
 		{
 			if (*index == 1024)
-			{
-				_write_buffer(buffer, index);
+			{	_write_buffer(buffer, index);
 				reset_buffer(buffer);
 				*index = 0;
 			}
@@ -34,13 +32,11 @@ int _printf(const char *format, ...)
 		if (format[i] == '\0')
 			break;
 		if (format[i] == '%')
-		{
-			i++;
+		{	i++;
 			for (j = 0; spec[j].tp != '\0'; j++)
 			{
 				if (format[i] == spec[j].tp)
-				{
-					spec[j].f(valist, buffer, index);
+				{	spec[j].f(valist, buffer, index);
 					break;
 				}
 			}
